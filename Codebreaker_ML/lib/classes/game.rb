498b0fc519cd19617f8874df6519e3beffff0dc5
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-03 21:05:52 +0200

    Implement #save_history

--- Codebreaker_ML/lib/classes/game.rb
+++ Codebreaker_ML/lib/classes/game.rb
@@ -1,10 +1,20 @@
+require 'yaml'
+require 'pry'
+
 module Codebreaker
   class Game
     attr_reader :code_size, :guess_history
     TEXT = {
-      rules:      'GAME RULES:',
+      rules:      'GAME RULES:
+      The code-breaker then gets some number of chances to break the code.
+      In each turn, the code-breaker makes a guess of four numbers.
+      The code-maker then marks the guess with up to four + and - signs.
+      A + indicates an exact match: one of the numbers in the guess is
+      the same as one of the numbers in the secret code and in the same position.
+      A - indicates a number match: one of the numbers in the guess is
+      the same as one of the numbers in the secret code but in a different position.',
       make_guess: 'Make guess:',
-      incorect:   'Incorect input',
+      incorrect:  'Input Error',
       hint:       'Enter "hint" for request a hint',
       quit:       'Enter "quit" to exit the game',
       quit?:      'Enter "yes" to exit the game',
@@ -12,7 +22,8 @@ module Codebreaker
       restart?:   'Enter "yes" to play again',
       win:        'Congratulations, you win the game',
       lose:       'Sorry you lose',
-      save:       'Enter "yes" to save game history'
+      save:       'Enter "yes" to save game history',
+      initials:   'Enter your name'
     }.freeze
 
     def initialize(code_size = 4, number_of_turns = 15)
@@ -48,6 +59,21 @@ module Codebreaker
     def status
       return :win if @guess_history.last == @secret_code
       :lose if @number_of_turns <= 0
+    end
+
+    def show_history
+      lines = ["Secret code was: #{@secret_code}"]
+      @guess_history.each_with_index do |guess, index|
+        lines.push "attempt: #{index} guess: #{guess} "
+      end
+      lines
+    end
+
+    def save_history(user)
+      user = Time.now.strftime('%d-%m-%Y-%H-%M') if user == ''
+      file_path = 'game_history/' + user + '.yaml'
+      File.open(file_path, 'w') { |f| f.write(YAML.dump(show_history)) }
+      file_path
     end
 
     private
