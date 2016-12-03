3d34d5217d6d2e85cbfef28b2979e46d441bfc7e
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-03 21:03:39 +0200

    Refactor

--- Codebreaker_ML/spec/codebreaker/views/game_terminal_spec.rb
+++ Codebreaker_ML/spec/codebreaker/views/game_terminal_spec.rb
@@ -5,22 +5,11 @@ module Codebreaker
   RSpec.describe GameTerminal do
     let(:game) { GameTerminal.new }
 
-    context '#start' do
-      it 'generate new wiev for game' do
-        # expect(game).to receive(:puts).with(/RULES/)
-        # expect(game).to receive(:puts).with(/Make guess/)
-        # # expect(game).to receive(:puts).with(/Sorry you lose/)
-        # game.stub(:gets).and_return('1111')
-        # game.stub(:gets).and_return('2222')
-        # game.start
-      end
-    end
-
     context '#text' do
       it 'puts game messages' do
         expect { game.text(:rules) }.to output(/RULES/).to_stdout
         expect { game.text(:make_guess) }.to output(/Make guess/).to_stdout
-        expect { game.text(:incorect) }.to output(/Incorect/).to_stdout
+        expect { game.text(:incorrect) }.to output(/Input Error/).to_stdout
         expect { game.text(:hint) }.to output(/hint/).to_stdout
         expect { game.text(:quit) }.to output(/quit/).to_stdout
         expect { game.text(:restart) }.to output(/restart/).to_stdout
@@ -33,19 +22,19 @@ module Codebreaker
     context '#action' do
       it 'check if user input if a system command [quit]' do
         expect(game).to receive(:puts).with('Enter "yes" to exit the game')
-        game.stub(:gets).and_return("no")
+        game.stub(:gets).and_return('no')
         game.action('quit')
       end
 
       it 'check if user input if a system command [restart]' do
         expect(game).to receive(:puts).with('Enter "yes" to play again')
-        game.stub(:gets).and_return("no")
+        game.stub(:gets).and_return('no')
         game.action('restart')
       end
 
       it 'check if user input if a system command [hint] and get it ' do
         expect(game).to receive(:puts).with(/[1-6]/)
-        game.stub(:gets).and_return("no")
+        game.stub(:gets).and_return('no')
         game.action('hint')
       end
     end
@@ -67,30 +56,17 @@ module Codebreaker
     end
 
     context '#save_history' do
-      it 'ask to save game history' do
+      it 'ask user to save game history' do
         expect(game).to receive(:puts).with('Enter "yes" to save game history')
         game.stub(:gets).and_return('no')
         game.save_history
       end
     end
 
-    context '#game_over' do
-      it 'generate new wiev for game'
-    end
-
     context '#show_history' do
       it 'show game process history' do
-        # expect(game).to receive(:puts).with(/RULES/)
-        # expect(game).to receive(:puts).with(/Make guess/)
-        # game.start
-        # expect(game).to receive(:puts).with(/Secret code was:/)
-        # game.show_history
+        expect { game.show_history }.to output(/Secret code was:/).to_stdout
       end
-    end
-
-    context '#save_history' do
-      it 'request player name'
-      it 'save game history to file'
     end
 
     context '#mark_guess' do
